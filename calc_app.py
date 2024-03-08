0d67b055016c28fa26478559ad3e3aa2d5e6c960
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-03-08 18:39:33 +0200

    full app

--- calc_app.py
+++ calc_app.py
@@ -1,22 +1,27 @@
-from flask import Flask,render_template , redirect
-from calc_class import Calculator
+from flask import Flask,render_template , redirect ,request
+
 
 
 app =  Flask(__name__)
 
-calculator = Calculator()
+
 
 @app.route('/')
 def home():
     return render_template('homepage.html') 
 
-@app.route('/calculate',methods = ['POST'] )
+@app.route('/calculate', methods=['POST'])
 def calculate():
-    pass
+    expression = request.form['screen']
+    try:
+        result = eval(expression)
+        print(result)
+        return render_template('homepage.html' ,result = result , expr = expression)
+    except Exception as e:
+        error_message = f"Error: {str(e)}"
+        return redirect('/')
+
     
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True,port=9000)
