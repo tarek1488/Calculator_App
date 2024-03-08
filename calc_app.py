9fb63360831f7cab5316caba4500401b7532878e
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-03-08 17:33:18 +0200

    front end

--- calc_app.py
+++ calc_app.py
@@ -1,26 +1,22 @@
 from flask import Flask,render_template , redirect
+from calc_class import Calculator
 
 
 app =  Flask(__name__)
 
+calculator = Calculator()
+
 @app.route('/')
 def home():
-    return "hello from home page" 
+    return render_template('homepage.html') 
 
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/calculate',methods = ['POST'] )
+def calculate():
+    pass
+    
 
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True,port=9000)
--- calc_class.py
+++ calc_class.py
@@ -1,27 +1,24 @@
 class Calculator:
     def __init__(self):
-        pass
+        self.experssion =  ''
+        return
     @classmethod
     def add(cls,x , y):
-        """
-        function the take two numbers and return the sum of them 
-
-        Args:
-            x (int): first number
-            y (int): second number
-
-        Returns:
-            int : the sum of x and  y 
-        """
         return x + y 
+    
     @classmethod
     def subtract(cls,x , y):
         return x - y
+    
     @classmethod
     def multiply(cls,x , y):
         return x * y
+    
     @classmethod
     def divide(cls,x , y):
         if y == 0:
             return "invalid devision by zero"
-        return x / y
+        return x / y 
+    
+    def set_experssion(self,experssion):
+        self.experssion = experssion
